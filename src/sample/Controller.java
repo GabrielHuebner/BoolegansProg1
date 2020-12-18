d92560d0bd241409199e317158a246ae54830a4a
c2010475105 <gabrielhuebner@gmx.at>
2020-12-18 13:40:20 +0100

    FirstPush, Main, Controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,12 @@
 package sample;
 
 public class Controller {
+
+    public Controller(){
+
+    }
+
+    public void what(){
+        System.out.print("Wut");
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +13,15 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("QuizGame");
+
+        Button button = new Button("Answer A");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 1000, 600);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 

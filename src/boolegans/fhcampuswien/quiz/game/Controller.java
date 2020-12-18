fe6ca598181beed68fd93943587521a3b966ce18
c2010475105 <gabrielhuebner@gmx.at>
2020-12-18 18:27:24 +0100

    Basic GUI, Button sample Action

--- src/boolegans/fhcampuswien/quiz/game/Controller.java
+++ src/boolegans/fhcampuswien/quiz/game/Controller.java
@@ -1,12 +1,32 @@
 package boolegans.fhcampuswien.quiz.game;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+import java.awt.event.ActionEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable{
+
+    @FXML
+    Button buttonA;
 
     public Controller(){
 
     }
 
-    public void what(){
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    public void what(javafx.event.ActionEvent actionEvent) {
         System.out.print("Wut");
+        buttonA.setStyle("-fx-background-color: green;");
     }
 }
--- src/boolegans/fhcampuswien/quiz/game/Main.java
+++ src/boolegans/fhcampuswien/quiz/game/Main.java
@@ -10,23 +10,18 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("QuizGame");
-
-        Button button = new Button("Answer A");
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        Scene scene = new Scene(layout, 1000, 600);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
+
+        Scene scene = new Scene(root);
+
+        primaryStage.setTitle("QuizGame");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 }
